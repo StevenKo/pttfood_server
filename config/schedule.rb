7a53b0eeb1f7ed9c12d5ab54808cbea8ddd047f0
Steven Ko <chunyuko85@gmaill.com>
2013-04-18 11:29:09 +0800

    add plist

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 env :PATH, ENV['PATH']
 
 
-every :day, :at => '00:00am,03:00pm' do
+every :day, :at => '10:13am,03:00pm' do
   rake 'crawl:crawl_ptt_new_articles',:output => {:error => 'log/error.log', :standard => 'log/cron.log'}
 end
